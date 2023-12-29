730af8fee89839a0c10cd259fa34b2d3de72c468
YimTaekeun <ytk030305@gmail.com>
2023-12-29 18:27:18 +0900

    보류

---
+++ src/b_1629/Main.java
@@ -0,0 +1,12 @@
+package b_1629;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Deque<Integer> deque = new ArrayDeque<>();
+        String target = scanner.next();
+        int howMany = scanner.nextInt();
+        for(int i = 0; i < target.length(); i++) deque.addLast(Integer.parseInt(target.substring(i, i + 1)));
+    }
+}
