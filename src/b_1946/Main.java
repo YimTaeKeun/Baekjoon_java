18fe784db4433e29838250507a424a02ed7d41ab
YimTaekeun <ytk030305@gmail.com>
2024-01-03 14:41:58 +0900

    Failed

--- src/b_1946/Main.java
+++ src/b_1946/Main.java
@@ -1,28 +1,30 @@
 package b_1946;
 
 import java.io.*;
+import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for(int t = Integer.parseInt(br.readLine()); t > 0; t--){
-            int[][] cal = new int[Integer.parseInt(br.readLine())][2];
+            List<Integer[]> list = new ArrayList<>(), list2 = new ArrayList<>();
             int cnt = 0;
-            for(int i = 0; i < cal.length; i++){
+            for(int i = Integer.parseInt(br.readLine()); i > 0; i--){
                 String[] temp = br.readLine().split(" ");
-                cal[i][0] = Integer.parseInt(temp[0]);
-                cal[i][1] = Integer.parseInt(temp[1]);
+                list.add(new Integer[] {Integer.parseInt(temp[0]), Integer.parseInt(temp[1])});
             }
-            for(int i = 0; i < cal.length; i++) if(isPick(cal, i)) cnt++;
+            list2.addAll(list);
+            list.sort((x, y) -> {return (x[0] - y[0]);});
+            list2.sort((x, y) -> {return (x[1] - y[1]);});
+            boolean can = true;
+            for(int i = 0; i < list.size(); i++){
+                for(int j = 0; j < i; j++) can = isPick(list2, list2.indexOf(list.get(i)), list.get(j));
+                if(can) cnt++;
+            }
             System.out.println(cnt);
         }
     }
-    public static boolean isPick(int[][] cal, int pos){
-        int[] thisInform = cal[pos];
-        for(int i = 0; i < cal.length; i++){
-            if(pos != i){
-                if(cal[i][0] < thisInform[0] && cal[i][1] < thisInform[1]) return false;
-            }
-        }
+    public static boolean isPick(List<Integer[]> list2, int pos, Integer[] target){
+        if(list2.indexOf(target) < pos) return false;
         return true;
     }
 }
