62977d9987e9c6755b34e9aaf3b3dc674635adcc
YimTaekeun <ytk030305@gmail.com>
2024-01-03 22:07:20 +0900

    Failed

--- src/b_1464/Main.java
+++ src/b_1464/Main.java
@@ -5,43 +5,45 @@ public class Main {
     static boolean isDecrease = false;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> deque = new ArrayDeque<>();
         String target = scanner.nextLine();
+        boolean onBreak = false;
         while(true){
-            stack.clear();
-            int pos = turnTarget(stack, target);
+            deque.clear();
+            int pos = turnTarget(deque, target);
             if(pos == -1) break;
             String temp = target;
             target = "";
-            int n = stack.size();
-            for(int i = 0; i < n; i++) target += Character.toString(stack.pop());
+            int n = deque.size();
+            for(int i = 0; i < n; i++) target += Character.toString(deque.pollLast());
+            if(target.length() > 0 && temp.substring(pos).length() > 0 && target.charAt(0) < temp.substring(pos).charAt(0) && target.charAt(target.length() - 1) > temp.substring(pos).charAt(0)) onBreak = true;
             target += temp.substring(pos);
-            if(pos == temp.length() - 1 && isDecrease) break;
+            if(onBreak) break;
         }
         System.out.println(target);
         scanner.close();
     }
-    public static int turnTarget(Stack<Character> stack, String string){
+    public static int turnTarget(Deque<Character> deque, String string){
         isDecrease = true;
         boolean nowDecrease, flag = false;
         int pos = 0;
         for(; pos < string.length(); pos++){
             char pushTarget = string.charAt(pos);
             if(pos == 0) {
-                stack.push(pushTarget);
+                deque.addFirst(pushTarget);
                 continue;
             }
-            nowDecrease = stack.peek() > pushTarget;
+            nowDecrease = deque.peekLast() > pushTarget;
             if(!flag) {
                 if(!nowDecrease) isDecrease = false;
                 else isDecrease = true;
                 flag = true;
-                if(stack.peek() == pushTarget) flag = false;
-                stack.push(pushTarget);
+                if(deque.peekLast() == pushTarget) flag = false;
+                deque.addLast(pushTarget);
             }
-            else if(stack.peek() == pushTarget) stack.push(pushTarget);
+            else if(deque.peekLast() == pushTarget) deque.addLast(pushTarget);
             else if((isDecrease && !nowDecrease) || (!isDecrease && nowDecrease)) return pos;
-            else stack.push(pushTarget);
+            else deque.addLast(pushTarget);
         }
         if(isDecrease) return pos;
         else return -1;
