78e02e9025ff3bab99ed5406f17ca04d620acdee
YimTaekeun <ytk030305@gmail.com>
2024-01-21 00:19:12 +0900

    Success

--- src/b_7568/Main.java
+++ src/b_7568/Main.java
@@ -1,37 +1,28 @@
 package b_7568;
 
 import java.io.*;
-import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer[]> cal = new ArrayList<>();
-        int t = Integer.parseInt(br.readLine());
-        for(int i = 0; i < t; i++){
-            String[] temp = br.readLine().split(" ");
-            cal.add(new Integer[] {Integer.parseInt(temp[0]), Integer.parseInt(temp[1]), i, 0});
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int humanCnt = Integer.parseInt(br.readLine());
+        Integer[][] people = new Integer[humanCnt][2];
+        String[] temp;
+        for(int i = 0; i < humanCnt; i++){
+            temp = br.readLine().split(" ");
+            people[i][0] = Integer.parseInt(temp[0]);
+            people[i][1] = Integer.parseInt(temp[1]);
         }
-        Collections.sort(cal, new Comparator<Integer[]>() {
-           @Override
-           public int compare(Integer[] target1, Integer[] target2){
-            if(target1[0] > target2[0] && target1[1] > target2[1]) return -1;
-            else if(target1[0] < target2[0] && target1[1] < target2[1]) return 1;
-            else return 0;
-           } 
-        });
-        int ranking = 1;
-        cal.get(0)[3] = 1;
-        for(int i = 1; i < cal.size(); i++){
-            Integer[] prev = cal.get(i - 1), now = cal.get(i);
-            if(now[0] < prev[0] && now[1] < prev[1]) {
-                cal.get(i)[3] = i + 1;
-                ranking = i + 1;
+        for(int i = 0; i < humanCnt; i++){
+            int ranking = 1;
+            for(int j = 0; j < humanCnt; j++){
+                if(i != j){
+                    if(people[i][0] < people[j][0] && people[i][1] < people[j][1]) ranking++;
+                }
             }
-            else cal.get(i)[3] = ranking;
+            bw.write(ranking + " ");
         }
-        int[] rankArr = new int[cal.size()];
-        for(Integer[] each: cal) rankArr[each[2]] = each[3];
-        for(Integer each: rankArr) System.out.print(each + " ");
-        System.out.println();
+        bw.write("\n");
+        bw.flush();
     }
 }
