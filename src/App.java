de98011870ef17cc8673f148fccfbd41aaeca406
YimTaekeun <ytk030305@gmail.com>
2024-01-28 08:39:59 +0900

    Failed

--- src/App.java
+++ src/App.java
@@ -1,6 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        char a = 'A';
-        System.out.println((int) a);
+        System.out.print("┌");
+        for(int i = 0; i < 8; i++) System.out.print("┬");
+        System.out.println();
+        System.out.print("├");
+        System.out.print("OX");
+        for(int i = 0; i < 4; i++) System.out.print("┼");
+        System.out.println();
+        System.out.print("├");
+        // System.out.print("OX");
+        for(int i = 0; i < 4; i++) System.out.print("┼");
     }
 }
---
+++ src/b_1043/Main.java
@@ -0,0 +1,10 @@
+package b_1043;
+
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer[]> events = new LinkedList<>();
+    }
+}
