26ed329465b75bcd9f43794731b6e280d043e96b
YimTaeKeun <ytk030305@gmail.com>
2025-11-03 18:57:39 +0900

    2018번 성공. 생각보다 무식하게 풀 때가 최적의 답일 때가 있다.

--- src/b_2018/Main.java
+++ src/b_2018/Main.java
@@ -2,45 +2,33 @@ package b_2018;
 
 import java.util.*;
 import java.io.*;
-// 생각보다 무식하게 풀어보기
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        long N = Long.parseLong(br.readLine());
-        if(N == 1 || N == 2) bw.write("1");
-        else{
-            List<Long> list = new ArrayList<>();
-            list.add(0L);
-            long result = 1;
-            long max = sumOneToN(N);
-            for(long i = 1; i <= N/2 + 1; i++) list.add(sumOneToN(i));
-            for(int i = 0; i < list.size(); i++){
-                long value = list.get(i);
-                long searchTargetValue = value + N;
-                if(searchTargetValue > max) continue;
-
-                if(isThereInArr(list, searchTargetValue)) result++;
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N + 1];
+        int result = 0;
+        for (int i = 0; i < N + 1; i++) arr[i] = i;
+        for(int i = 1; i < N + 1; i++){
+            if(arr[i] == N){
+                result++;
+                continue;
             }
-            bw.write(String.valueOf(result));
+            long sum = arr[i];
+            for(int j = i + 1; j < N + 1; j++){
+                if(sum + arr[j] == N){
+                    result++;
+                    break;
+                }
+                else if(sum + arr[j] > N) break;
+                sum += arr[j];
+            }
         }
+        bw.write(result+"\n");
         bw.flush();
-    }
-    public static long sumOneToN(long N) {
-        return N * (N + 1) / 2;
-    }
-    public static boolean isThereInArr(List<Long> arr, long value){
-        // binary search
-        int sIdx = 0;
-        int eIdx = arr.size() - 1;
-        while(true){
-            if(sIdx > eIdx) return false;
 
-            int mid = (sIdx + eIdx)/2;
-            if(arr.get(mid) == value) return true;
-            if(arr.get(mid) > value) eIdx = mid-1;
-            else if(arr.get(mid) < value) sIdx = mid+1;
-        }
     }
 }
